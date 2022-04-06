65878a998ffc7d52a8a34fe3f7b3b84ad83e02ff
elias <eliaslogachoflores@gmail.com>
2022-04-06 12:55:03 +0200

    Diseño de Ventanas: Iniciar Sesion, Principal, Inserciones, Busqueda(Sin terminar): Elias

--- PDyRH_WS/PDyRH/src/vista/VPrincipal.java
+++ PDyRH_WS/PDyRH/src/vista/VPrincipal.java
@@ -1,11 +1,18 @@
 package vista;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.Font;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.UIManager;
 import javax.swing.border.EmptyBorder;
 
 public class VPrincipal extends JDialog {
@@ -29,26 +36,59 @@ public class VPrincipal extends JDialog {
 	 * Create the dialog.
 	 */
 	public VPrincipal() {
-		setBounds(100, 100, 450, 300);
+		setTitle("Principal");
+		setBounds(100, 100, 416, 312);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
 		{
-			JPanel buttonPane = new JPanel();
-			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
-			getContentPane().add(buttonPane, BorderLayout.SOUTH);
-			{
-				JButton okButton = new JButton("OK");
-				okButton.setActionCommand("OK");
-				buttonPane.add(okButton);
-				getRootPane().setDefaultButton(okButton);
-			}
-			{
-				JButton cancelButton = new JButton("Cancel");
-				cancelButton.setActionCommand("Cancel");
-				buttonPane.add(cancelButton);
-			}
+			JButton btnInsertar = new JButton("INSERTAR");
+			btnInsertar.setFont(new Font("Tahoma", Font.PLAIN, 13));
+			btnInsertar.setBounds(38, 75, 123, 55);
+			contentPanel.add(btnInsertar);
+		}
+		
+		JButton btnBusqueda = new JButton("BUSQUEDA");
+		btnBusqueda.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		btnBusqueda.setBounds(212, 75, 123, 55);
+		contentPanel.add(btnBusqueda);
+		
+		JButton btnGestionar = new JButton("GESTIONAR");
+		btnGestionar.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		btnGestionar.setBounds(38, 161, 123, 55);
+		contentPanel.add(btnGestionar);
+		
+		JButton btnComparar = new JButton("COMPARAR");
+		btnComparar.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		btnComparar.setBounds(212, 161, 123, 55);
+		contentPanel.add(btnComparar);
+		
+		JButton btnBusqRapida = new JButton("Busqueda rapida");
+		btnBusqRapida.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		btnBusqRapida.setBounds(231, 239, 147, 23);
+		contentPanel.add(btnBusqRapida);
+		
+		{
+			JMenuBar menuBar = new JMenuBar();
+			menuBar.setBorderPainted(false);
+			menuBar.setBackground(UIManager.getColor("FormattedTextField.selectionBackground"));
+			menuBar.setBounds(0, 0, 400, 37);
+			contentPanel.add(menuBar);
+			
+			JMenu menUsuario = new JMenu("Usuario");
+			menUsuario.setHorizontalAlignment(SwingConstants.LEFT);
+			menUsuario.setFont(new Font("Dialog", Font.PLAIN, 14));
+			menUsuario.setBackground(new Color(0, 0, 255));
+			menUsuario.setForeground(Color.BLACK);
+			menuBar.add(menUsuario);
+			
+			JMenuItem mCerrar = new JMenuItem("Cerrar Sesion");
+			mCerrar.setHorizontalAlignment(SwingConstants.TRAILING);
+			mCerrar.setBackground(new Color(32, 178, 170));
+			mCerrar.setFont(new Font("Tahoma", Font.PLAIN, 12));
+			mCerrar.setForeground(Color.BLACK);
+			menUsuario.add(mCerrar);
 		}
 	}
 
